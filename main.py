4e8d10a0c84536c3f1e9164076ceff92bb1d91de
carlospuigserveer <91721643+carlospuigserver@users.noreply.github.com>
2022-05-12 07:50:55 +0000

    Obtengo análisis EDA navegación

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from conversaciones import filas
 from conversaciones import columnas
 from conversaciones import moda
@@ -10,10 +12,17 @@ from conversaciones import q1
 from conversaciones import q3
 from conversaciones import desv
 from conversaciones import mediana
-import pandas as pd
 
 conversaciones=pd.read_csv("conversaciones.csv")
+
+
 navegacion=pd.read_csv("navegación.csv")
+
+
+
+
+
+#CONVERSACIONES
 
 # Numero filas
 filasC=filas(conversaciones)
@@ -71,3 +80,71 @@ a= conversaciones["id_lead"].quantile(0.25)
 b= conversaciones["id_lead"].quantile(0.75)
 rango_int= b-a
 print("El rango intercuartílico es {}".format(rango_int))
+
+
+
+
+
+
+
+
+#NAVEGACIÓN
+
+
+# Numero filas
+filasN=filas(navegacion)
+filasN.operacion()
+
+#Num column
+columnasN=columnas(navegacion)
+columnasN.operacion()
+
+#Valores max
+max_N = max(navegacion,"user_recurrent")
+max.operacion()
+
+#Valores min
+min_N = min(navegacion,"user_recurrent")
+min.operacion()
+
+
+#Media
+media_N = media(navegacion,"user_recurrent")
+media.operacion()
+
+#Moda 
+moda_N = moda(navegacion,"user_recurrent")
+moda.operacion()
+
+#Mediana
+mediana_N = mediana(navegacion,"user_recurrent")
+mediana.operacion()
+
+#Desviación típica
+desv_típ_N = desv(navegacion,"user_recurrent")
+desv.operacion()
+
+
+#Varianza
+varianza_N = varianza(navegacion,"user_recurrent")
+varianza.operacion()
+
+#Rango
+Rango_N = rango(navegacion,"user_recurrent")
+rango.operacion()
+
+
+#Q1
+q1_N = q1(navegacion,"user_recurrent")
+q1.operacion()
+
+#Q3
+q3_N = q3(navegacion,"user_recurrent")
+q3.operacion()
+
+# Rango intercuartílico
+a= navegacion["user_recurrent"].quantile(0.25)
+b= navegacion["user_recurrent"].quantile(0.75)
+rango_int= b-a
+print("El rango intercuartílico es {}".format(rango_int))
+
